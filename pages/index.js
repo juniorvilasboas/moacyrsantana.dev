3a3abce184724b4370bf9901faa6cc0b00aecd6e
Moacyr Santana <junior.vilasboas@gmail.com>
2021-12-10 21:47:39 +0000

    Aperfeiçoando o layout

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,29 @@ import getUser from '../utils/getUser'
 
 const Index = ({ repos, user }) => {
   return (
-    <div className='container mx-auto'>
-      <h1 className='text-5xl'>Olá, eu sou o Moacyr Santana</h1>
-      <p>Github stats: Public Repos: {user.public_repos} / Public Gists: {user.public_gists} / Followers: {user.followers}</p>
+    <div className='container mx-auto border'>
+      <div className='grid grid-cols-2 pt-16 mx-auto leading-none w-2/3'>
+        <div>
+          <h1 className='text-2xl uppercase'>Hi! I'm</h1>
+          <h1 className='text-2xl uppercase font-bold'>Moacyr Santana</h1>
+          <h2 className='text-2xl'>Software Engineer</h2>
+          <h3>Contact</h3>
+          <ul>
+            <li>Linkedin</li>
+            <li>Github</li>
+            <li>Youtube</li>
+          </ul>
+          <p>Github stats: Public Repos: {user.public_repos} / Public Gists: {user.public_gists} / Followers: {user.followers}</p>
+        </div>
+        <div>
+          <img className='esquerda h-64 pt-4 ml-28' src='/images/foto.png' />
+        </div>
+      </div>
+      <div className='bg-white rounded-xl shadow-lg mx-auto w-4/5 py-12'>
+        <h3 className='text-3xl text-center'>What I do</h3>
+        <p className='text-2xl pt-6 px-16 font-bold'>Fullstack Developer asdlkfjaslkdfjçaslkdfjçslakdfjçsdlk</p>
+        <p className='text-2xl pt-6 px-16 font-bold'>Fullstack Developer asdlkfjaslkdfjçaslkdfjçslakdfjçsdlk</p>
+      </div>
       <h2 className='font-bold text-3xl'>Meus repositórios no Github</h2>
       {repos.map(repo => {
         return (
