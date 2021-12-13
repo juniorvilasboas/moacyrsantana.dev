fa07c4f3cf21e5b0ee16c185127a8759742b07e6
Moacyr Santana <junior.vilasboas@gmail.com>
2021-12-13 13:15:24 +0000

    Atualizando layout e add menu

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,61 @@
 import React from 'react'
 import getUser from '../utils/getUser'
+import { FaLinkedin, FaGithub, FaYoutube } from 'react-icons/fa';
+import { FiLink } from 'react-icons/fi';
 
 const Index = ({ repos, user }) => {
   return (
-    <div className='container mx-auto border'>
-      <div className='grid grid-cols-2 pt-16 mx-auto leading-none w-2/3'>
-        <div>
-          <h1 className='text-2xl uppercase'>Hi! I'm</h1>
-          <h1 className='text-2xl uppercase font-bold'>Moacyr Santana</h1>
-          <h2 className='text-2xl'>Software Engineer</h2>
-          <h3>Contact</h3>
-          <ul>
-            <li>Linkedin</li>
-            <li>Github</li>
-            <li>Youtube</li>
-          </ul>
-          <p>Github stats: Public Repos: {user.public_repos} / Public Gists: {user.public_gists} / Followers: {user.followers}</p>
+    <div className='container mx-auto'>
+      <nav id='menu' className='p-4'>
+        <h3 className='float-left ml-36'>Personal</h3>
+        <ul>
+          <li className='mr-10'><a className='text-dourado' href='/'>Home</a></li>
+          <li className='mr-10'><a href='/repositorios'>Repositorio</a></li>
+          <li className='mr-10'><a href='/portifolios'>Portifolio</a></li>
+          <li className='mr-20 border-2 border-dourado py-2 px-4 rounded-xl'><a href='/contact'>Contact</a></li>
+        </ul>
+      </nav>
+      <div className='grid grid-cols-2 pt-8 mx-auto leading-none w-2/3'>
+        <div className='pt-20'>
+          <h1 className='text-2xl uppercase pl-14 -mb-2'>Hi! I'm</h1>
+          <h1 className='text-dourado text-2xl uppercase font-bold pl-14 -mb-2 dourado'>Moacyr Santana</h1>
+          <h2 className='text-2xl pl-14'>Software Engineer</h2>
+          <div className='relative border border-dourado rounded-xl px-14 pb-4 pt-10 mt-6'>
+            <h3 className='absolute rounded-xl bg-dourado text-white py-2 px-6 top-0 -mt-5 text-xl font-bold uppercase'>Contact me</h3>
+            <p>
+              <a href='http://www.linkedin.com/in/moacyrsantana' target='_black'>< FaLinkedin className='text-5xl inline-block mr-6'/></a>
+              <a href='https://github.com/juniorvilasboas' target='_black'>< FaGithub className='text-5xl inline-block mr-6'/></a>
+              <a href='https://www.youtube.com/channel/UC17gkRYV_f6qZJz6SBC9-lg' target='_black'>< FaYoutube className='text-5xl inline-block mr-6'/></a>
+              <a href='http://www.linkedin.com/in/moacyrsantana' target='_black'>< FiLink className='text-5xl inline-block mr-6'/></a>
+              <br />
+              <span className='inline-block mt-4'>or drop a line: junior.vilasboas@gmail.com</span>
+            </p>
+          </div>
         </div>
-        <div>
-          <img className='esquerda h-64 pt-4 ml-28' src='/images/foto.png' />
+        <div className='realtive'>
+          <img className='absolute ml-20 h-80' src='/images/back-network.png' />
+          <img className='esquerda h-96 pt-4 ml-28' src='/images/foto.png' />
         </div>
       </div>
-      <div className='bg-white rounded-xl shadow-lg mx-auto w-4/5 py-12'>
-        <h3 className='text-3xl text-center'>What I do</h3>
+      <div className='rounded-xl bg-white shadow-lg mx-auto w-4/5 py-12'>
+        <h3 className='text-dourado text-3xl text-center'>What I do</h3>
         <p className='text-2xl pt-6 px-16 font-bold'>Fullstack Developer asdlkfjaslkdfjçaslkdfjçslakdfjçsdlk</p>
         <p className='text-2xl pt-6 px-16 font-bold'>Fullstack Developer asdlkfjaslkdfjçaslkdfjçslakdfjçsdlk</p>
       </div>
+
+      <div>
+        <h3 className=' text-dourado text-2xl font-bold text-center uppercase mt-10 mb-5'>My Education</h3>
+        <div className='grid grid-cols-2 bg-white w-4/5 mx-auto leading-none rounded-xl shadow-lg py-6'>
+          {[1,2].map( i => (
+            <div className='border-dashed border-l px-12'>
+              <h4 className='text-dourado text-lg uppercase font-bold'>Master's Degree</h4>
+              <p className='text-xl uppercase'>Computer Sience <br />
+              <span className='text-lg normal-case font-bold'>UNIFACS - University of Salvador</span></p>
+            </div>
+          ))}
+        </div>
+      </div>
+
       <h2 className='font-bold text-3xl'>Meus repositórios no Github</h2>
       {repos.map(repo => {
         return (
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
+  purge: ['./src/**/*.js'],
   theme: {
     fontFamily: {
       sans: ['"Titillium Web"']
+    },
+
+    extend: {
+      colors: {
+        dourado: '#D6B21F'
+      }
     }
   }
 }
