948ade8e455ee9c1a94714aa8ed421152260aab1
Moacyr Santana <junior.vilasboas@gmail.com>
2021-12-14 13:14:13 +0000

    Atualizações do layout e tornando a app em uma unica pagina

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,24 @@
 import React from 'react'
+import Head from 'next/head';
 import getUser from '../utils/getUser'
-import { FaLinkedin, FaGithub, FaYoutube } from 'react-icons/fa';
-import { FiLink } from 'react-icons/fi';
+import { FaLinkedin, FaGithub, FaYoutube, FaStar } from 'react-icons/fa';
+import { FiLink, FiUsers } from 'react-icons/fi';
+import { GoGist, GoRepo } from 'react-icons/go';
+import { SiJavascript, SiHtml5, SiNodedotjs, SiReact, SiCss3 } from 'react-icons/si';
 
 const Index = ({ repos, user, year }) => {
   return (
-    <div>
+    <div id='home'>
+      <Head>
+        <title>Moacyr Santana - Fullstack Developer</title>
+      </Head>
       <nav id='menu' className='p-4 border-b-2'>
         <h3 className='float-left ml-56 text-2xl'>Personal</h3>
         <ul>
-          <li className='mr-10'><a className='text-dourado' href='/'>Home</a></li>
-          <li className='mr-10'><a href='/repositorios'>Repositorio</a></li>
-          <li className='mr-10'><a href='/portifolios'>Portifolio</a></li>
-          <li className='mr-20 border-2 border-dourado py-2 px-4 rounded-xl'><a href='/contact'>Contact</a></li>
+          <li className='mr-6'><a href='#home'>Home</a></li>
+          <li className='mr-6'><a href='#education'>My Education</a></li>
+          <li className='mr-6'><a href='#repos'>Repositories</a></li>
+          <li className='mr-6'><a href='#port'>Portfolio</a></li>
         </ul>
       </nav>
       <div className='container mx-auto'>
@@ -40,24 +46,76 @@ const Index = ({ repos, user, year }) => {
           </div>
         </div>
         <div className='rounded-xl bg-white shadow-lg mx-auto w-4/5 py-12'>
-          <h3 className='text-dourado text-3xl text-center'>What I do</h3>
-          <p className='text-2xl pt-6 px-16 font-bold'>Fullstack Developer asdlkfjaslkdfjçaslkdfjçslakdfjçsdlk</p>
-          <p className='text-2xl pt-6 px-16 font-bold'>Fullstack Developer asdlkfjaslkdfjçaslkdfjçslakdfjçsdlk</p>
+          <h3 className='text-dourado text-3xl text-center mb-8'>What I do</h3>
+          <div className='grid grid-cols-2 bg-white w-4/5 mx-auto'>
+            <div className='relative border-2 rounded-xl shadow-lg px-14 pb-4 pt-10 mt-6'>
+              <h3 className='absolute text-xl py-1 px-6 top-0 ml-6 -mt-6 bg-white border-2 inline-block rounded-xl font-bold'>Techs</h3>
+              <p className='grid grid-cols-4'>
+                <a href='#' target='_blank' className='mb-4'><SiJavascript className='text-3xl hover:text-dourado inline-block' /></a>
+                <a href='#' target='_blank'><SiReact className='text-3xl hover:text-dourado inline-block mr-6' /></a>
+                <a href='#' target='_blank'><SiHtml5 className='text-3xl hover:text-dourado inline-block mr-6' /></a>
+                <a href='#' target='_blank'><SiNodedotjs className='text-3xl hover:text-dourado inline-block mr-6' /></a>
+                <a href='#' target='_blank'><SiCss3 className='text-3xl hover:text-dourado inline-block mr-6' /></a>
+                <a href='#' target='_blank'><FaGithub className='text-3xl hover:text-dourado inline-block mr-6' /></a>
+              </p>
+            </div>
+            <div>
+              <p className='text-2xl pt-6 px-16 font-bold'>Fullstack Developer asdlkfjaslkdfjçaslkdfjçslakdfjçsdlk</p>
+              <p className='text-2xl pt-6 px-16 font-bold'>Fullstack Developer asdlkfjaslkdfjçaslkdfjçslakdfjçsdlk</p>
+            </div>
+          </div>
         </div>
 
-        <div>
-          <h3 className=' text-dourado text-2xl font-bold text-center uppercase mt-10 mb-5'>My Education</h3>
+        <div id='education'>
+          <h3 className=' text-dourado text-2xl font-bold text-center uppercase mt-16 mb-5'>My Education</h3>
           <div className='grid grid-cols-2 bg-white w-4/5 mx-auto leading-none rounded-xl shadow-lg py-6'>
-            {[1,2].map( i => (
-              <div className='border-dashed border-l px-12'>
-                <h4 className='text-dourado text-lg uppercase font-bold'>Master's Degree</h4>
-                <p className='text-xl uppercase'>Computer Sience <br />
-                <span className='text-lg normal-case font-bold'>UNIFACS - University of Salvador</span></p>
+            <div className='border-dashed border-l px-12 my-4'>
+              <h4 className='text-dourado text-lg uppercase font-bold'>University graduate</h4>
+              <p className='text-xl uppercase'>Computer Sience <br />
+              <span className='text-lg normal-case font-bold hover:underline'><a href='https://www.unifacs.br/' target='_blank'>UNIFACS - University of Salvador</a></span></p>
+            </div>
+            <div className='border-dashed border-l px-12 my-4'>
+              <h4 className='text-dourado text-lg uppercase font-bold'>Cursos</h4>
+              <p className='text-xl uppercase'>Fullstack Master <br />
+              <span className='text-lg normal-case font-bold hover:underline'><a href='https://devpleno.com/' target='_blank'>DevPleno</a></span></p>
+            </div>
+          </div>
+        </div>
+
+        {/** Repositorios */}
+        <div id='repos'>
+          <h3 className='text-dourado text-2xl font-bold text-center uppercase mt-16'>Github Repositories</h3>
+          <p className='text-center mb-5'>Github stats: <GoRepo className='inline-block' /> {user.public_repos} / <GoGist className='inline-block' /> {user.public_gists} / <FiUsers className='inline-block' /> {user.followers}</p>
+          <div className='grid grid-cols-3 bg-white w-4/5 mx-auto leading-none shadow-lg rounded-xl py-6'>
+            {repos.map(repo => {
+              return (
+                <div className='rounded-xl'>
+                  <div key={repo.id} className='rounded-xl bg-gray-200 mx-4 my-2 p-4 hover:shadow-md'>
+                    <h3 className='font-bold hover:underline'><a href={'http://github.com/'+repo.full_name} target='_blank'>{repo.full_name}</a></h3>
+                    <p>Language: {repo.language} / <FaStar className='inline-block' />Stars: {repo.stargazers_count}</p>
+                  </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+
+        {/** Portifolios */}
+        <div id='port'>
+          <h3 className='text-dourado text-2xl font-bold text-center uppercase mt-16 mb-5'>Portfolios</h3>
+          <div className='grid grid-cols-3 bg-white w-4/5 mx-auto leading-none shadow-lg rounded-xl p-6'>
+            {[1,2,3,4].map( i => (
+              <div className='px-12 shadow-lg w-4/5 mx-auto my-4 rounded-xl'>
+                <a href='' target='_blank' className='w-full'>
+                  <img src='/images/layout-base.png' className='w-full' />
+                  <h4 className='text-center my-5'>Nome do site</h4>
+                </a>
               </div>
             ))}
           </div>
         </div>
       </div>
+
 
       <div className='footer-basic mt-10 border-t-2 drop-shadow-[0_10px_3px_rgba(0,0,0,0.25)]'>
         <footer>
@@ -67,10 +125,10 @@ const Index = ({ repos, user, year }) => {
               <a href="https://www.facebook.com/junior.vilasboas/" target='_blank'><i className='icon ion-social-facebook'></i></a>
             </div>
             <ul className='list-inline'>
-                <li className='list-inline-item inline-block'><a href="/" className='hover:text-dourado'>Home</a></li>
-                <li className='list-inline-item inline-block'><a href="/repositorios" className='hover:text-dourado'>Repositorios</a></li>
-                <li className='list-inline-item inline-block'><a href="/portifolios" className='hover:text-dourado'>Portifolios</a></li>
-                <li className='list-inline-item inline-block'><a href="/contact" className='hover:text-dourado'>Contact</a></li>
+                <li className='list-inline-item inline-block'><a href="#home" className='hover:text-dourado'>Home</a></li>
+                <li className='list-inline-item inline-block'><a href="#education" className='hover:text-dourado'>My Education</a></li>
+                <li className='list-inline-item inline-block'><a href="#repos" className='hover:text-dourado'>Repositories</a></li>
+                <li className='list-inline-item inline-block'><a href="#port" className='hover:text-dourado'>Portfolios</a></li>
             </ul>
             <p className='copyright'>Serveware Sistemas © { year }</p>
         </footer>
@@ -80,13 +138,13 @@ const Index = ({ repos, user, year }) => {
 }
 
 export async function getServerSideProps(context) {
-  //const { repos, user } = await getUser('juniorvilasboas')
+  const { repos, user } = await getUser('juniorvilasboas')
 
   return {
     props: {
       year: new Date().getFullYear(),
-      //repos,
-      //user
+      repos,
+      user
     }
   }
 }
