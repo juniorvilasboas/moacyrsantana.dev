24335efdbf95dc31a1b80a1f6c428523ccb7c3c3
Moacyr Santana <junior.vilasboas@gmail.com>
2022-05-13 01:24:14 +0100

    update Alert

--- components/Alert.tsx
+++ components/Alert.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
 interface Props {
-  children: React.ReactNode
-  showIcon: boolean
+  children: string
+  showIcon?: boolean
 }
 
 const Alert = ({ children, showIcon = true }: Props) => {
