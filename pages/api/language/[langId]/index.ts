ca3d98f5bba4e27c163bf8c9f2a4eda6f6bfdbfc
Moacyr Santana <junior.vilasboas@gmail.com>
2023-05-05 01:01:51 +0100

    fix

--- pages/api/language/[langId]/index.ts
+++ pages/api/language/[langId]/index.ts
@@ -19,8 +19,11 @@ const Language = async (
     const languageItem = await getLanguageById(langId)
 
     if (languageItem) {
+      //@ts-ignore
       languageData.levelUnderstand = { id: languageData.levelUnderstand}
+      //@ts-ignore
       languageData.levelSpeak = { id: languageData.levelSpeak}
+      //@ts-ignore
       languageData.levelWrite = { id: languageData.levelWrite}
 
       const savedLanguage = await update(langId, languageData)
