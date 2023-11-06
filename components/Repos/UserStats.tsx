f2f9ef05fc98518ccbb1c62696b545f243cdeb77
Moacyr Santana <junior.vilasboas@gmail.com>
2023-11-06 20:44:54 +0000

    fix gogist

--- components/Repos/UserStats.tsx
+++ components/Repos/UserStats.tsx
@@ -1,12 +1,12 @@
 import { FiUsers } from 'react-icons/fi'
-import { GoGist, GoRepo } from 'react-icons/go'
+import { GoGitBranch, GoRepo } from 'react-icons/go'
 
 const UserStats = ({ user }: any) => {
   return (
     <p className='text-center mb-5'>
       Github stats:
       <GoRepo className='inline-block' /> {user?.public_repos} /
-      <GoGist className='inline-block' /> {user?.public_gists} /
+      <GoGitBranch className='inline-block' /> {user?.public_gists} /
       <FiUsers className='inline-block' /> {user?.followers}
     </p>
   )
